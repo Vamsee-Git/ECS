b4ae93cb21c1d9f82b944c42554bcac26f0ee66b
Vamsee-Git <95101547+Vamsee-Git@users.noreply.github.com>
2025-02-23 22:46:33 +0530

    Update backend.tf

--- infrastructure/backend.tf
+++ infrastructure/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "my-terraform-state-bucket-two-tier-vamsee"
     key            = "terraform/statefile_ecs"
-    region         = "us-east-1"
+    region         = "ap-south-1"
     dynamodb_table = "terraform-lock"
   }
 }
