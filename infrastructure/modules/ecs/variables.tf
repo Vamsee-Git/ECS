59a69635409734061fa64ac123b50679a3e425f0
Vamsee-Git <95101547+Vamsee-Git@users.noreply.github.com>
2025-02-24 00:05:11 +0530

    Update variables.tf

--- infrastructure/modules/ecs/variables.tf
+++ infrastructure/modules/ecs/variables.tf
@@ -16,13 +16,11 @@ variable "private_subnets" {
 variable "patientservice_image" {
   description = "Docker image for Patient Service"
   type        = string
-  default     = "711387104213.dkr.ecr.us-east-1.amazonaws.com/dev-ecr:patient-app-a603480f1ae993122cb95270e4ff9a3afbff2ca3"
 }
 
 variable "appointmentservice_image" {
   description = "Docker image for Appointment Service"
   type        = string
-  default    = "711387104213.dkr.ecr.us-east-1.amazonaws.com/dev-ecr:appointment-app-a603480f1ae993122cb95270e4ff9a3afbff2ca3"
 }
 
 variable "patientservice_target_group_arn" {
