9b5ae95fecc8bcb712e97367fffff2be2e25a7ff
Vamsee-Git <95101547+Vamsee-Git@users.noreply.github.com>
2025-02-24 13:39:46 +0530

    Update main.tf

--- infrastructure/modules/vpc/main.tf
+++ infrastructure/modules/vpc/main.tf
@@ -52,3 +52,32 @@ resource "aws_route_table_association" "public" {
   subnet_id      = aws_subnet.public[count.index].id
   route_table_id = aws_route_table.public.id
 }
+resource "aws_eip" "NAT_eip" {
+  domain     = "vpc"
+  depends_on = [aws_internet_gateway.gw]
+}
+ 
+resource "aws_nat_gateway" "nat_gw" {
+  allocation_id = aws_eip.NAT_eip.id
+  subnet_id     = aws_subnet.public[0].id
+  depends_on    = [aws_eip.NAT_eip]
+}
+ 
+ 
+ 
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.main.id
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat_gw.id
+  }
+  tags = {
+    Name = "${var.environment}-private-rt"
+  }
+}
+ 
+resource "aws_route_table_association" "private" {
+  count          = length(var.private_subnets)
+  subnet_id      = aws_subnet.private[count.index].id
+  route_table_id = aws_route_table.private.id
+}
